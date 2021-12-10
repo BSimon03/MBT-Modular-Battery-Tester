2a7e9026a58c1bf6aca1779da15ee36e4d141dfa
BSimon03 <90346844+BSimon03@users.noreply.github.com>
2021-12-10 14:02:34 +0100

    master SPI configured, main updated

--- BatteryTesterMaster/src/init_ATmega32u4.h
+++ BatteryTesterMaster/src/init_ATmega32u4.h
@@ -10,3 +10,75 @@
 #include <stdio.h>
 #include <stdint.h>
 
+//Bit defines
+//SPI
+#define MISO		PINB3		//Serial Data Input Pin
+#define MOSI		PINB2		//Serial Data Output Pin
+#define SCL		    PINB1		//Clock Pin -> Output
+
+#define SS			PINB0		//Chip Select/Slave Select Pin... to be pulled down when data shifting is complete
+
+//Status
+#define STAT_RED	PINA1		//Red status LED
+#define STAT_GREEN	PINA2		//Green status LED
+
+//Clock Prescaler
+#define CLK_PRESCALER_VALUE 1  //Must be 1, 2, 4, 8, 16, 32, 64, 128 or 256
+
+/* MCU CLOCK PRESCALER */
+
+//Prescaler value converted to bit settings.
+
+#if CLK_PRESCALER_VALUE == 1
+#define CLK_PS_SETTING (1<<CLKPCE)
+
+#elif CLK_PRESCALER_VALUE == 2
+#define CLK_PS_SETTING (1<<CLKPCE)|(1<<CLKPS0)
+
+#elif CLK_PRESCALER_VALUE == 4
+#define CLK_PS_SETTING (1<<CLKPCE)|(1<<CLKPS1)
+
+#elif CLK_PRESCALER_VALUE == 8
+#define CLK_PS_SETTING (1<<CLKPCE)|(1<<CLKPS1)|(1<<CLKPS0)
+
+#elif CLK_PRESCALER_VALUE == 16
+#define CLK_PS_SETTING (1<<CLKPCE)|(1<<CLKPS2)
+
+#elif CLK_PRESCALER_VALUE == 32
+#define CLK_PS_SETTING (1<<CLKPCE)|(1<<CLKPS2)|(1<<CLKPS0)
+
+#elif CLK_PRESCALER_VALUE == 64
+#define CLK_PS_SETTING (1<<CLKPCE)|(1<<CLKPS2)|(1<<CLKPS1)
+
+#elif CLK_PRESCALER_VALUE == 128
+#define CLK_PS_SETTING (1<<CLKPCE)|(1<<CLKPS2)|(1<<CLKPS1)|(1<<CLKPS0)
+
+#elif CLK_PRESCALER_VALUE == 256
+#define CLK_PS_SETTING (1<<CLKPCE)|(1<<CLKPS3)
+
+#else
+#error Invalid prescaler setting.
+
+#endif
+
+void mcu_set_clock()					    //Function to easily switch between Clock prescalers using the previous define
+{
+	CLKPR = CLK_PRESCALER_VALUE;
+}
+
+void SPI_setup()
+{
+	//port settings for master
+	DDRB|=(1<<MOSI)|(1<<SS)|(1<<SCL);       //Setting direction of PB1
+	DDRB&=~(1<<MISO);
+	PORTB|=(1<<MISO);
+	
+	
+    SPCR = (1<<SPE)|(1<<MSTR)|(1<<SPR0);    //Enable SPI, Master, set clock rate fck/16 
+}
+
+void init_atmega32u4()					    //Combining all setup functions
+{
+	mcu_set_clock();
+	SPI_setup();
+}
--- BatteryTesterMaster/src/main.cpp
+++ BatteryTesterMaster/src/main.cpp
@@ -11,127 +11,39 @@
 #include "init_ATmega32u4.h"
 #include "MBT_settings.h"
 
-//Bit defines
-//SPI
-#define SDI  PINB0
-#define SDO  PINB1
-#define USCK PINB2
-#define CS	 PINB6
+//Data received
+uint8_t storedDATA = 0;
 
-//Constants
-const char not_selected=0x00;
-const char request_info=0x2A;
-const char send_second_string=0xE4;
+//Data to be sent
+uint8_t information_string; //Bits[7:6] for indication and information, Bits[5:0] resistance of the battery cell
+uint8_t value_string;		//Bits[7:0] capacitance of the battery cell
 
-const char slave_count=8;
-
-//data
-char send[8]={0};
-char received[8];
-
-int capacity[8]={0}, resistance[8]={0};		//discharge amphours and resistance
-	
 //Interrupt
-volatile unsigned long t=0;
-volatile unsigned long millis=0;	//32bit: Overflow after ~48 days
+uint8_t timer_counter = 0;
+uint32_t millis = 0; //32bit: Overflow after ~48 days
 
-void mcu_set_clock()
-{
-	//internal clock must not be set, its selected by default
-	CLKPR|=(1<<CLKPCE); //Clock prescaler enabled
-	CLKPR&=~(1<<CLKPS3)|(1<<CLKPS2)|(1<<CLKPS1)|(1<<CLKPS0); //no prescaler (all 0), but still avoids unintentional clock changes
-}
+uint32_t t = 0; //millis compare value
 
-void SPI_setup()
-{
-	//port settings for master
-	DDRB|=(1<<SDO)|(1<<CS)|(1<<USCK);		//CS as output, CLK as output
-	PORTB|=(1<<CS);							//CS pulled to high
-	
-	DDRB&=~(1<<SDI);
-	PORTB|=(1<<SDI);  // Pull-up
-	
-	//Choosing SPI aka three wire mode p.133
-	USICR&=~(1<<USIWM1);
-	USICR|=(1<<USIWM0);
-	
-	//set as master, generates clock p.135
-	USICR|=(1<<USITC);			
-	
-	//Select clock source: Software clock strobe USICLK p.134
-	USICR|=(1<<USICLK);			
-	USICR&=~(1<<USICS0)|(1<<USICS1);
-}
+//Measurements
+uint16_t charge_capacity = 0;
+uint16_t discharge_capacity = 0;
+uint8_t discharge_resistance = 0;
 
-char check_slave(char s_address)
-{
-	/*empty byte*/
-	int i=0;
-	for(i=0; i>slave_count-s_address; i++)
-	USIDR=not_selected;
-	
-	/*request*/
-	USIDR=request_info;
-	i++;
-	
-	/*empty byte*/
-	for(i; i>slave_count; i++)
-	USIDR=not_selected;
-	
-	/*Pull Down */
-	PORTB&=~(1<<CS);
-}
+uint8_t battery_temperature = 0;
+float battery_voltage = 0;
 
-float receive_float(char s_address, char request)
-{
-	
-}
-
-void send_data()
-{
-	
-}
-
-void timer_setup()
-{
-	//Timer 0: 1ms
-	TCCR0A|=(1<<CTC0);	//CTC0
-	TCCR0A&=~(1<<ICEN0)|(1<<TCW0);
-	TCCR0B|=(1<<CS02)|(1<<CS00);
-	TCCR0B&=~(1<<CS01);
-	
-	TIMSK|=(1<<OCIE0A);
-	OCR0A=20;
-}
-
-void delay_ms(unsigned int ms)
-{
-	unsigned long t = millis;
-	while((t+ms)>=millis){}
-}
+//Outcome of Measurements
+uint8_t capacity = 0;
+uint8_t resistance = 0;
 
 int main(void)
 {
-	//DO out, DI in
-	//sck in, cs in
-	
-	DDRA
-	DDRB
-	//
-	
-	set_mcu_clock();
-	SPI_setup();
-	timer_setup();
-	
-	sei();		//global interrupt enable
+	init_atmega32u4(); //Initiating the MCU, Registers configurated
+
+	sei(); //global interrupt enable
 
 	while (1)
 	{
 		
 	}
-}
-
-ISR(TIMER0_COMPA_vect)
-{
-	millis++;	//count +1 every ms
 }
