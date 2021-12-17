c221e437d79f77e2d73055155662ffdf24766cda
BSimon03 <90346844+BSimon03@users.noreply.github.com>
2021-12-17 19:23:23 +0100

    Update MBT_settings.h

--- Software/BatteryTesterMaster/include/MBT_settings.h
+++ Software/BatteryTesterMaster/include/MBT_settings.h
@@ -20,12 +20,12 @@
 #define ADC_OFFSET		0.25					//ADC shottky offset
 
 //Strings sent by the master -> Requests
-const uint8_t request_info= 0xAA; 				//0b11001100		//Master requests information
+const uint8_t request_info= 0xCC; 				//0b11001100		//Master requests information
 const uint8_t request_secs= 0xF0; 				//0b11110000		//Master requests the second byte of information, after the finish is acknowledged
 
 //Answer string templates
 const uint8_t error_high_temp 	= 0b01100000;	//high temperature (>=60°C)		send temperature /2
-const uint8_t error_low_volt 	= 0b00100000;	//low voltage (<=2,5V)			send voltage x 10
+const uint8_t error_low_volt 	= 0b00100000;	//low voltage (<2,5V)			send voltage x 10
 
 const uint8_t error_not_suit	= 0b01000000;	//not suitable
 const uint8_t error_res_high 	= 0b01001000;	//res too high (res>=50mOhm)
